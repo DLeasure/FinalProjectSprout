723e13e0b0b8716848f361b92fb7e3b2f649a2c7
Jessica Billingslea <jessicrr@gmail.com>
2019-08-28 21:22:23 -0400

    home page component

---
+++ src/app/sprout-home/sprout-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sprout-home',
+  templateUrl: './sprout-home.component.html',
+  styleUrls: ['./sprout-home.component.css']
+})
+export class SproutHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
