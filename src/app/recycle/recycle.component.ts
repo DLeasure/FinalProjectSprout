d27cafb4bada9c87c023a7078333c876579c5431
DLeasure <DLeasure9@gmail.com>
2019-09-03 20:18:56 -0400

    finished recycle API

--- src/app/recycle/recycle.component.ts
+++ src/app/recycle/recycle.component.ts
@@ -16,6 +16,7 @@ export class RecycleComponent implements OnInit {
   locationId: string;
 
   response: object;
+  responseArray: Array<object>;
   responseDescription: string;
   responseDistance: number;
   responseAddress: string;
@@ -31,7 +32,7 @@ export class RecycleComponent implements OnInit {
 
   requestLatLonAndLocation() {
     this.earthService.getUsersLatLon(this.country, this.ZIP).subscribe(resp => {
-      console.log(resp);
+      // console.log(resp);
       this.latitude = resp.latitude;
       this.longitude = resp.longitude;
       this.requestLocation();
@@ -40,10 +41,10 @@ export class RecycleComponent implements OnInit {
 
   requestLocation() {
     // this.requestLatLon();
-    console.log("first API request ran");
+    // console.log("first API request ran");
     this.earthService.getLocationFromLatLon(this.latitude, this.longitude).subscribe(resp => {
-      console.log("second API request ran");
-      console.log(resp);
+      // console.log("second API request ran");
+      // console.log(resp);
       this.responseDescription = resp.description;
       this.responseDistance = resp.distance;
       this.locationId = resp.location_id;
@@ -55,15 +56,25 @@ export class RecycleComponent implements OnInit {
   requestLocationDetails() {
     // this.requestLatLon();
     this.earthService.getLocationDetails(this.locationId).subscribe(resp => {
-      console.log("third API request ran");
-      console.log(resp);
+      // console.log("third API request ran");
+      // console.log(resp);
       // locationIdValue = this.locationId;
       this.response = resp.valueOf(this.locationId);
-      this.responseAddress = this.response.Q1RQNVVfXVxHUQ.address;
-      console.log(this.responseAddress);
+      this.responseArray = Object.keys(this.response).map(i => this.response[i]);
+      // console.log(this.responseArray);
+      this.responseAddress = this.responseArray[0].address;
+      this.responseDescription = this.responseArray[0].description;
+      this.responseDistance = this.responseArray[0].distance;
+      this.responseCity = this.responseArray[0].city;
+      this.responseZip = this.responseArray[0].postal_code;
+      this.responseState = this.responseArray[0].province;
+      this.responsePhone = this.responseArray[0].phone;
+      this.responseCountry = this.responseArray[0].country;
+      this.responseHours = this.responseArray[0].hours;
+      // console.log(this.responseAddress);
       // this.responseDescription = resp.description;
       // this.responseDistance = resp.distance;
-    });
+    }); 
   };
   
 
