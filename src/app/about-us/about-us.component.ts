d17933fcfc80a4b12039cb60908213b3641a9731
Jessica Billingslea <jessicrr@gmail.com>
2019-09-14 19:21:22 -0400

    set toggle for team info

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -9,8 +9,15 @@ import { InteractiveFlowers } from '../animations/interactive-flowers'
 export class AboutUsComponent implements OnInit {
 
   constructor() { }
+  
+  message: string = 'View All';
+  jonInfoShown: boolean = false;
+  daveInfoShown: boolean = false;
+  jessInfoShown: boolean = false;
+  
 
   ngOnInit() {
+    
     const canvas = <HTMLCanvasElement>document.getElementById('flower');
     const flowers = new InteractiveFlowers(canvas);
     
@@ -18,7 +25,30 @@ export class AboutUsComponent implements OnInit {
     btn.addEventListener('click', () => {
       flowers.clearCanvas();
     });
-    
-      }
-    
+  }
+
+  toggleJon(){
+    this.jonInfoShown = !this.jonInfoShown;
+  };
+
+  toggleDave(){
+    this.daveInfoShown = !this.daveInfoShown;
+  };
+
+  toggleJess(){
+    this.jessInfoShown = !this.jessInfoShown;
+  };
+
+  viewAll(){
+    this.jonInfoShown = !this.jonInfoShown;
+    this.daveInfoShown = !this.daveInfoShown;
+    this.jessInfoShown = !this.jessInfoShown;
+
+    if(this.jonInfoShown && this.daveInfoShown && this.jessInfoShown) {
+      this.message = 'Close All';
     }
+    else {
+      this.message = 'View All';
+    }
+  };
+}
