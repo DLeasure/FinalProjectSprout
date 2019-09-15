5e4d476171d52e523364eec0feaddfca21a9babe
jon plumb <jplumb89@outlook.com>
2019-09-15 15:20:08 -0400

    added quiz and made it work

--- src/app/recycle/recycle.component.ts
+++ src/app/recycle/recycle.component.ts
@@ -2,28 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { EarthService } from '../earth.service';
 import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { forkJoin, Observable } from 'rxjs';
-
 @Component({
   selector: 'app-recycle',
   templateUrl: './recycle.component.html',
   styleUrls: ['./recycle.component.css']
 })
-
 export class RecycleComponent implements OnInit {
-
   country: string = "US";
   ZIP: number;
   latitude: number;
   longitude: number;
   locationId: string;
-
   response: object;
   responseArrayKeys: Array<string> = [];
   responseArrayObjects: Array<object> = [];
   errorMessage: string = "";
-
   constructor(private earthService: EarthService) {};
-  
   recyclingFacts: Array<string> = [
     "Without exception, recycling is the top action society can do to simultaneously improve the environment, the economy, sustainable manufacturing, and to prevent waste from going into oceans.",
     "Recycling is in a crisis in the U.S. due to public confusion about recycling. Find your closest recycling center below!",
@@ -32,14 +26,11 @@ export class RecycleComponent implements OnInit {
     "When U.S. recycling levels reach 75% it will generate 1.5 million new jobs in the U.S. (net). Let's find your closest center below!",
     "Manufacturers truly want these materials back to reuse in their manufacturing, but they aren't able to reuse the materials if people don't recycle. Let's find your closest location and get started!"
   ]
-
   recyclingFact: string = "";
-  
-
   requestLatLonAndLocation() {
     if (this.ZIP > 4999 && this.ZIP < 100000) {
       // console.log("searching for " + this.ZIP);
-      this.earthService.getUsersLatLon(this.country, this.ZIP).subscribe(resp => {
+      this.earthService.getUsersLatLon(this.country, this.ZIP).subscribe((resp: any) => {
         if (resp !== null) {
           // console.log(resp);
           this.latitude = resp.latitude;
@@ -53,7 +44,6 @@ export class RecycleComponent implements OnInit {
       this.errorMessage = "Please enter a 5-digit number to proceed";
     };
   };
-
   requestLocation() {
     this.earthService.getLocationFromLatLon(this.latitude, this.longitude).subscribe(resp => {
       // console.log("second API request ran");
@@ -65,7 +55,6 @@ export class RecycleComponent implements OnInit {
       this.requestLocationDetails();
     });
   };
-
   requestLocationDetails() {
     // console.log(this.responseArrayKeys[0]);
     // console.log("fork runs now");
@@ -102,11 +91,11 @@ export class RecycleComponent implements OnInit {
       console.log(this.responseArrayObjects);
     });
   };
-  
-
   ngOnInit() {
     let integerForRecyclingFact = Math.floor(Math.random() * this.recyclingFacts.length);
     this.recyclingFact = this.recyclingFacts[integerForRecyclingFact];
   }
+}
 
-} 
+
+
