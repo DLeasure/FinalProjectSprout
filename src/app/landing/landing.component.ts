01ac741749fb971997a83724f0024c52e4316e35
DLeasure <DLeasure9@gmail.com>
2019-09-15 21:14:38 -0400

    updating landing component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -15,76 +15,83 @@ export class LandingComponent implements OnInit {
 
   constructor() { }
 
-  Rotate() {
-    var element = document.getElementById('image');
-    var x = document.getElementById("reduce");
-    var a = document.getElementById("reuse");
-    var b = document.getElementById("recycle");
-    var x1 = document.getElementById("reduce1");
-    var a1 = document.getElementById("reuse1");
-    var b1 = document.getElementById("recycle1");
+  activeFacts : string = "Facts about reusing";
 
-    if (element.className === "normal") {
-      element.className = "rotate";
-      var x = document.getElementById("reduce");
-      if (x.style.display === "none") {
-        x.style.display = "block";
-        a.style.display = "none";
-        b.style.display = "none";
-        x1.style.display = "block";
-        a1.style.display = "none";
-        b1.style.display = "none";
-      } else {
-        x.style.display = "none";
-        a.style.display = "block";
-        b.style.display = "none";
-        x1.style.display = "none";
-        a1.style.display = "block";
-        b1.style.display = "none";
-      }
-    }
-    else if (element.className === "rotate") {
-      element.className = 'rotate1';
-      var a = document.getElementById("reuse");
-      if (x.style.display === "none") {
-        a.style.display = "block";
-        x.style.display = "none";
-        b.style.display = "none";
-        a1.style.display = "block";
-        x1.style.display = "none";
-        b1.style.display = "none";
-      } else {
-        x.style.display = "none";
-        a.style.display = "block";
-        b.style.display = "none";
-        x1.style.display = "none";
-        a1.style.display = "block";
-        b1.style.display = "none";
-      }
-      a.style.display === "block";    
-    } else if ( element.className === "rotate1" && element.className ==="rotate1") {
-      element.className = 'normal';
-      var b = document.getElementById("recycle");
-      if (x.style.display === "none") {
-        b.style.display = "block";
-        a.style.display = "none";
-        x.style.display = "none";
-        b1.style.display = "block";
-        a1.style.display = "none";
-        x1.style.display = "none";
-      } else {
-        x1.style.display = "block";
-        a1.style.display = "none";
-        b1.style.display = "none";
-        x1.style.display = "block";
-        a1.style.display = "none";
-        b1.style.display = "none";
-      }
-    }
+  rotateTree(component) {
+
   }
-  optionClicked1() {
-    document.getElementById('image').click();
-  }
+
+  // Rotate() {
+  //   var element = document.getElementById('image');
+  //   var x = document.getElementById("reduce");
+  //   var a = document.getElementById("reuse");
+  //   var b = document.getElementById("recycle");
+  //   var x1 = document.getElementById("reduce1");
+  //   var a1 = document.getElementById("reuse1");
+  //   var b1 = document.getElementById("recycle1");
+
+  //   if (element.className === "normal") {
+  //     element.className = "rotate";
+  //     var x = document.getElementById("reduce");
+  //     if (x.style.display === "none") {
+  //       x.style.display = "block";
+  //       a.style.display = "none";
+  //       b.style.display = "none";
+  //       x1.style.display = "block";
+  //       a1.style.display = "none";
+  //       b1.style.display = "none";
+  //     } else {
+  //       x.style.display = "none";
+  //       a.style.display = "block";
+  //       b.style.display = "none";
+  //       x1.style.display = "none";
+  //       a1.style.display = "block";
+  //       b1.style.display = "none";
+  //     }
+  //   }
+  //   else if (element.className === "rotate") {
+  //     element.className = 'rotate1';
+  //     var a = document.getElementById("reuse");
+  //     if (x.style.display === "none") {
+  //       a.style.display = "block";
+  //       x.style.display = "none";
+  //       b.style.display = "none";
+  //       a1.style.display = "block";
+  //       x1.style.display = "none";
+  //       b1.style.display = "none";
+  //     } else {
+  //       x.style.display = "none";
+  //       a.style.display = "block";
+  //       b.style.display = "none";
+  //       x1.style.display = "none";
+  //       a1.style.display = "block";
+  //       b1.style.display = "none";
+  //     }
+  //     a.style.display === "block";    
+  //   } else if ( element.className === "rotate1" && element.className ==="rotate1") {
+  //     element.className = 'normal';
+  //     var b = document.getElementById("recycle");
+  //     if (x.style.display === "none") {
+  //       b.style.display = "block";
+  //       a.style.display = "none";
+  //       x.style.display = "none";
+  //       b1.style.display = "block";
+  //       a1.style.display = "none";
+  //       x1.style.display = "none";
+  //     } else {
+  //       x1.style.display = "block";
+  //       a1.style.display = "none";
+  //       b1.style.display = "none";
+  //       x1.style.display = "block";
+  //       a1.style.display = "none";
+  //       b1.style.display = "none";
+  //     }
+  //   }
+  // }
+
+  // optionClicked1() {
+  //   document.getElementById('image').click();
+  // }
   ngOnInit() {
        }
     
