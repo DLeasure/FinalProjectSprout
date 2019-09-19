3033293bea21bd28a9f7a2ce7bd4b552347b3673
jon plumb <jplumb89@outlook.com>
2019-09-18 21:21:11 -0400

    treeeeeeeee quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -22,6 +22,52 @@ export class QuizComponent implements OnInit {
 
   ngOnInit() {
     this.initalize();
+  }
+  
+  initalize() {
+    this._displayTitle = true;
+    this._quizInfoService.getJson().subscribe(this.onSuccess.bind(this), this.onError.bind(this));
+    this.makeTree();
+  }
+
+  generateOrderForAnswers() {
+    let answers = this._questionAndAnswer.others;
+    answers.push(this._questionAndAnswer.answer);
+    const length = answers.length;
+    const randomOrder = [];
+    for (let i = 0; i < length; i++) {
+      const index = Math.floor(Math.random() * Math.floor(answers.length));
+      randomOrder.push(answers[index]);
+      answers = answers.filter(a => a !== answers[index]);
+    }
+    return randomOrder;
+  }
+
+  onSuccess(items: QuestionsAndAnswers[]) {
+    const final = items.map((res: any) => {
+    
+      return { id: res.id, question: res.question, answer: res.answer, others: res.others, answered: false, correct: false, image: 'img/html.png' };
+    });
+    this._questionsAndAnswers = final;
+  }
+
+  onError(error: any) {
+    console.log(error);
+  }
+
+  getQuestion(): QuestionsAndAnswers {
+    const nonUsed: QuestionsAndAnswers[] = this._questionsAndAnswers.filter(qanda => !qanda.answered);
+    const randomIndex = Math.floor(Math.random() * Math.floor(nonUsed.length));
+    const question = nonUsed[randomIndex];
+    return question;
+  }
+
+  getCorrectAmount() {
+    const correctQuestions = this._questionsAndAnswers.filter((item: QuestionsAndAnswers) => item.correct);
+    this._numberCorrect = correctQuestions.length;
+  }
+
+  makeTree() {
     (function () {
       const width = 400;
       const height = 420;
@@ -55,7 +101,7 @@ export class QuizComponent implements OnInit {
       }
     
       function get_random_length(start_length) {
-          return start_length - (3 + (Math.random() * 15));
+          return start_length - (4 + (Math.random() * 15));
       }
     
       function get_random_angle(start_angle, direction) {
@@ -147,47 +193,6 @@ export class QuizComponent implements OnInit {
         window.onload = main;
     
   })();
-  }
-  
-  initalize() {
-    this._displayTitle = true;
-    this._quizInfoService.getJson().subscribe(this.onSuccess.bind(this), this.onError.bind(this));
-  }
-
-  generateOrderForAnswers() {
-    let answers = this._questionAndAnswer.others;
-    answers.push(this._questionAndAnswer.answer);
-    const length = answers.length;
-    const randomOrder = [];
-    for (let i = 0; i < length; i++) {
-      const index = Math.floor(Math.random() * Math.floor(answers.length));
-      randomOrder.push(answers[index]);
-      answers = answers.filter(a => a !== answers[index]);
-    }
-    return randomOrder;
-  }
-
-  onSuccess(items: QuestionsAndAnswers[]) {
-    const final = items.map((res: any) => {
-      return { id: res.id, question: res.question, answer: res.answer, others: res.others, answered: false, correct: false, image: 'img/html.png' };
-    });
-    this._questionsAndAnswers = final;
-  }
-
-  onError(error: any) {
-    console.log(error);
-  }
-
-  getQuestion(): QuestionsAndAnswers {
-    const nonUsed: QuestionsAndAnswers[] = this._questionsAndAnswers.filter(qanda => !qanda.answered);
-    const randomIndex = Math.floor(Math.random() * Math.floor(nonUsed.length));
-    const question = nonUsed[randomIndex];
-    return question;
-  }
-
-  getCorrectAmount() {
-    const correctQuestions = this._questionsAndAnswers.filter((item: QuestionsAndAnswers) => item.correct);
-    this._numberCorrect = correctQuestions.length;
   }
 
   startQuiz() {
