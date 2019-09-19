79c29c323e337b009995e924183e7a5f10782d26
DLeasure <47162668+DLeasure@users.noreply.github.com>
2019-09-18 21:09:57 -0400

    Revert "Jonsbranch"

--- src/app/services/quiz-info.service.ts
+++ src/app/services/quiz-info.service.ts
@@ -7,7 +7,6 @@ import { QuestionsAndAnswers } from '../models/question-and-answers';
 @Injectable({
   providedIn: 'root'
 })
-
 export class QuizInfoService {
 
   questionsAndAnswers: QuestionsAndAnswers[];
